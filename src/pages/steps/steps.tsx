b4986792bce52b238e8d35de7822994d67e801bf
abienne <abienne@gmail.com>
2022-06-09 17:47:39 +0200

    feat(prismic): add prismic preview

--- src/pages/steps/steps.tsx
+++ src/pages/steps/steps.tsx
@@ -9,22 +9,23 @@ export const Steps = () => (
             <SList>
                 <SListItem><a target="_blank" href="https://github.com/vercel/next.js/tree/canary/examples/cms-prismic" rel="noopener noreferrer">prismic</a></SListItem>
                 <SListItem>prismic preview</SListItem>
-                <SListItem>icon sidebar + style</SListItem>
-                <SListItem>hero</SListItem>
-                <SListItem>prismic template</SListItem>
                 <SListItem>light mode</SListItem>
-                <SListItem>routing config</SListItem>
+                <SListItem><a target="_blank" href="https://nextjs.org/docs/advanced-features/i18n-routing" rel="noopener noreferrer">language</a></SListItem>
+                <SListItem>icon sidebar + style</SListItem>
                 <SListItem>github link</SListItem>
                 <SListItem>linkedin link</SListItem>
+                <SListItem>hero</SListItem>
+                <SListItem>prismic template</SListItem>
                 <SListItem>contact</SListItem>
                 <SListItem>stack</SListItem>
                 <SListItem>about me</SListItem>
+                <SListItem><a target="_blank" href="https://bholmes.dev/blog/a-shiny-on-hover-effect-that-follows-your-mouse-css/" rel="noopener noreferrer">glow</a></SListItem>
                 <SListItem>experiences</SListItem>
+                <SListItem>routing config</SListItem>
                 <SListItem>2 - 3 articles</SListItem>
                 <SListItem>page transition</SListItem>
                 <SListItem>useDebounce - /sophia-react/apps/global-search/src/hooks/useDebounce.js</SListItem>
                 <SListItem>infinite scroll - /sophia-react/apps/global-search/src/components/ModalSearch/components/useResultContent.js</SListItem>
-                <SListItem><a target="_blank" href="https://nextjs.org/docs/advanced-features/i18n-routing" rel="noopener noreferrer">language</a></SListItem>
                 <SListItem>search</SListItem>
                 <SListItem>deploy vercel</SListItem>
                 <SListItem>ci - test - docker</SListItem>
